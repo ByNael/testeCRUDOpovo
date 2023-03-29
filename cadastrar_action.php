6565f1c3cded08d8e465fcaa666171339e43b0b4
Natanael Lino <natannlino@gmail.com>
2023-03-29 18:52:44 -0300

    finalização da funcionalidade INSERT com validação

--- cadastrar_action.php
+++ cadastrar_action.php
@@ -5,10 +5,14 @@ require 'config.php';
 $nome = filter_input(INPUT_POST, 'nome');
 $email = filter_input(INPUT_POST, 'email');
 
-$sql = $pdo->prepare("INSERT INTO usuario (nome, email) VALUES (:nome, :email)");
-$sql->bindValue(':nome', $nome);
-$sql->bindValue(':email', $email);
-$sql->execute();
+if($nome && $email){
+    $sql = $pdo->prepare("INSERT INTO usuario (nome, email) VALUES (:nome, :email)");
+    $sql->bindValue(':nome', $nome);
+    $sql->bindValue(':email', $email);
+    $sql->execute();
+    
+    header("Location: index.php");
+    
+}
 
-header("Location: index.php");
 ?>
